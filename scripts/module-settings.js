6951e63ca4884e180a6f4cbf8a3f5ef6593b4043
Brian Stormont <mosttornbrain@gmail.com>
2024-08-02 21:58:07 -0400

    Add preliminary loot handlebar.

--- scripts/module-settings.js
+++ scripts/module-settings.js
@@ -95,6 +95,8 @@ Hooks.once('ready', () => {
           handlebarDefault = "modules/pf2e-md-exporter/handlebars/vehicle_handlebar.hbs";
         } else if (label == 'TYPES.Actor.army') {
           handlebarDefault = "modules/pf2e-md-exporter/handlebars/army_handlebar.hbs";
+        } else if (label == 'TYPES.Actor.loot') {
+          handlebarDefault = "modules/pf2e-md-exporter/handlebars/loot_handlebar.hbs";
         }
         game.settings.register(MOD_CONFIG.MODULE_NAME, `template.Actor.${type}`, {
             name: game.i18n.format(`${MOD_CONFIG.MODULE_NAME}.actorTemplate.Name`, {name: actorname}),
